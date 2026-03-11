4a9ae46c58f914e2de8c50e786860289c2070404
Sander Evers <sander.evers@topicus.nl>
2026-03-11 15:57:29 +0100

    fix build, hack browser.ts

--- src/browser.ts
+++ src/browser.ts
@@ -1,3 +1,4 @@
+/// <reference lib="dom" />
 /**
  * This file is the entrypoint of browser builds.
  * The code executes when loaded in a browser.
@@ -6,4 +7,5 @@ import * as index from './index';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).jio = index;  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 Object.assign((window as any).jio, index)  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
